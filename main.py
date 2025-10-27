3969b588d18c0ed85c874efe7aa9e787a7624513
Sven <sven.boor@transbits.nl>
2025-10-27 16:13:23 +0100

    Made indexes update dynamically in daily report.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import psycopg2
 import os
 import stats_pre_processor
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import logging
 
@@ -56,23 +56,37 @@ def update_materialized_views(cur):
     logging.info("Start refresh materialized view park_event_on_date")
     cur.execute("REFRESH MATERIALIZED VIEW park_event_on_date")
     logging.info("Finished refresh materialized view park_event_on_date")
+    
     logging.info("DROP INDEX IF EXISTS park_events_ended_less_than_three_days_ago")
+    cur.execute("DROP INDEX IF EXISTS idx_park_events_location_recent_gist;")
+    logging.info("Finished DROP INDEX idx_park_events_location_recent_gist")
+    
+    logging.info("DROP INDEX IF EXISTS park_events_ended_less_than_three_days_ago") 
     cur.execute("DROP INDEX IF EXISTS park_events_ended_less_than_three_days_ago;")
-    logging.info("Finished DROP INDEX park_events_ended_less_than_a_week_ago")
+    logging.info("Finished DROP INDEX park_events_ended_less_than_three_days_ago")
 
-    date_three_days_ago = (datetime.now() - timedelta(days=3)).date()
-    logging.info("Create new index on park_events starting from_date: {}".format(date_three_days_ago))
+
+    date_three_days_ago = (datetime.now(timezone.utc) - timedelta(days=3))
+    logging.info("Create new index on park_events starting from_date: %s", date_three_days_ago)
     stmt = """
-    CREATE INDEX park_events_ended_less_than_a_week_ago
-    ON park_events (start_time)
-    WHERE end_time >= %s OR end_time IS NULL;
+        CREATE INDEX park_events_ended_less_than_three_days_ago
+        ON park_events (end_time)
+        WHERE end_time >= %s OR end_time IS NULL;
     """
-    logging.info("Finished creating index")
     cur.execute(stmt, (date_three_days_ago,))
 
-update_materialized_views(cur)
+    stmt = """
+        CREATE INDEX IF NOT EXISTS idx_park_events_location_recent_gist
+        ON park_events
+        USING gist (location)
+        WHERE end_time >= %s OR end_time IS NULL;
+    """
+
+    cur.execute(stmt, (date_three_days_ago,))
+    logging.info("Finished creating index")
    
 
+update_materialized_views(cur)
 
 
 conn.commit()
